67fa0a50291a58453e7f66aa149634db24d3e5ae
Eduard de Torres <eduard.detorres@salle.url.edu>
2018-12-03 19:16:40 +0100

    FEAT - Inserir lista
    Implementada la funcionalidad "Insert" de la lista.
    Esta funcionalidad añade una nueva Destination a la lista.

--- model/list/list.c
+++ model/list/list.c
@@ -29,7 +29,61 @@ List create() {
 }
 
 void insert(List * l, Destination d) {
-	
+	int i;
+	int condicio;
+
+
+	//Demanem memòria pel nou node i comprovem si n'hem obtingut
+	Node * nou  = (Node *) malloc (sizeof (Node));
+
+	//Copiem l'element a inserir al nou node
+	nou->dest = d;
+
+	//Per cadascun dels camps d'ordenació, ordenarem el node
+	for (i = 0; i < MAX_SORTING; i++) {
+		//En primer lloc, situem el pdi al primer fantasma
+		l->poi[i] = l->first;
+
+		do {
+			//Avancem al següent element i mirem si aquest encara l'hem de saltar
+			l->poi[i] = l->poi[i]->next[i];
+
+			if (l->poi[i] = l->last) {
+				break;
+			}
+
+			//Les condicions per saltar un element depenen del mode d'ordenació
+			switch (i) {
+				case BY_NAME:
+					condicio = strcmp(d.name, l->poi[i]->dest.name) > 0;
+					break;
+				case BY_AVG_PRICE:
+					condicio = getAverageHotelPrice(d) > getAverageHotelPrice(l->poi[i]->dest);
+					break;
+				case BY_DISTANCE:
+					condicio = getDistanceFromBcn(d) > getDistanceFromBcn(l->poi[i]->dest);
+					break;
+				case BY_TIME:
+					condicio = getTimeFromBcn(d) > getTimeFromBcn(l->poi[i]->dest);
+					break;
+				default:
+					//Si per un casual algú decideix afegir més camps, ens protegim ordenant segons entrada
+					condicio = 0;
+					break;
+			}
+			//Seguim saltant mentre no arribem al darrer element i es segueixi complint la condició de salt
+		} while (condicio);
+
+		//Apuntem els punters del nou node
+		nou->prev[i] = l->poi[i]->prev[i];
+		nou->next[i] = l->poi[i];
+		//Fem que els elements als que hem apuntat ens apuntin a nosaltres
+		nou->prev[i]->next[i] = nou;
+		nou->next[i]->prev[i] = nou;
+		//Deixem el pdi sobre el nou element
+		l->poi[i] = nou;
+	}
+	l -> n++;
 }
 
 Destination readPoi(List l, int mode) {
