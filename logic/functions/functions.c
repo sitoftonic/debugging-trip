e67532ce8b045e2555a2f2ef7760a6f0d43e2e20
sitoftonic <sitoftonic@gmail.com>
2019-01-04 17:34:07 +0100

    fix: Fixed an incorrect error message for a correct value of input

--- logic/functions/functions.c
+++ logic/functions/functions.c
@@ -48,7 +48,7 @@ void planTrip(List *l){
         printPlanMenu();
         option = askUserForOption();
 
-		if (destination < 1 || destination > 2) {
+		if (option < 1 || option > 2) {
 			printOptionError();
 		}
     } while (option < 1 || option > 2);
