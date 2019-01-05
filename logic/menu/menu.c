4359f8afba4f95b9f5667a9de764c776a7debf21
sitoftonic <sitoftonic@gmail.com>
2019-01-05 13:59:33 +0100

    doc: Added comments to every function in menu.c

--- logic/menu/menu.c
+++ logic/menu/menu.c
@@ -6,10 +6,12 @@
 #define FIRST_OPTION 1
 #define LAST_OPTION 4
 
+// Funció que printa el missatge inicial del programa
 void printWelcomeLine() {
 	printf("\n\t---===  Welcome to TripOrganizer  ===---\n");
 }
 
+// Funció que printa el menu principal del programa
 void printMenu() {
 	printf("\n\t1. Show all destinations\n");
 	printf("\t2. Plan a trip\n");
@@ -17,10 +19,12 @@ void printMenu() {
 	printf("\n\t4. Exit\n\n");
 }
 
+// Funció que printa un error de rang de la opció
 void printOptionError() {
 	printf("\n\tError: Option out of range\n\n");
 }
 
+// Funció que demana una opció a l'usuari i la retorna
 int askUserForOption() {
 	int option;
 	printf("\tChoose an option: ");
@@ -29,14 +33,17 @@ int askUserForOption() {
 	return  option;
 }
 
+// Funció que retorna si la opció es o no correcta
 int isCorrect(int option) {
 	return option >= FIRST_OPTION && option <= LAST_OPTION;
 }
 
+// Funció que retorna si la opció escollida es la de sortida
 int isExit(int option) {
 	return option == LAST_OPTION;
 }
 
+// Funció que llegeix una string introduida per l'usuari
 char * readUserString() {
 	int i = 0;
 	int size = 10;
@@ -61,16 +68,19 @@ char * readUserString() {
 	return ret;
 }
 
+// Funció que demana el path de la database a l'usuari
 char * askUserForPath() {
 	printf("\tEnter the path of the new DB: ");
 
 	return readUserString();
 }
 
+// Funció que printa un error del fitxer
 void printFileError(char * file) {
 	printf("\n\tError: Couldn't load \"%s\"\n", file);
 }
 
+// Funció que printa el menu d'ordenació
 void printSortMenu() {
 	printf("\n\tHow do you wanted sorted?\n");
 	printf("\t\t1. By name\n");
@@ -79,6 +89,7 @@ void printSortMenu() {
 	printf("\t\t4. By time\n\n");
 }
 
+// Funció que printa la destinació
 void printDestination(Destination d, int pos, int option) {
 	printf("\n\t-------- %d --------\n", pos);
 	printf("\tName:\t\t%s\n", d.name);
@@ -99,11 +110,13 @@ void printDestination(Destination d, int pos, int option) {
 	}
 }
 
+// Funció que printa el menú de plan trip
 void printPlanMenu() {
 	printf("\n\t1. Show best hotels\n");
 	printf("\t2. Show best altitude route\n\n");
 }
 
+// Funció que printa el mapa d'altitud d'una destinació determinada
 void printAltitudeMap(char ** map, int n) {
 	int i, j;
 
@@ -117,6 +130,7 @@ void printAltitudeMap(char ** map, int n) {
 	}
 }
 
+// Funció que printa la llista de destinacions disponibles a la estructura
 void printDestinationList(List * l) {
 	int i = 0;
 	printf("\n\tSelect a destination:\n");
@@ -132,11 +146,12 @@ void printDestinationList(List * l) {
 
 }
 
+// Funció que printa l'average del preu dels hotels d'una destinació
 void printAverage(double average) {
 	printf("\n\t\tAverage: %lf\n\n", average);
 }
 
-
+// Funció que printa el top 3 d'hotels d'una destinació
 void printTop3(double * prices) {
 	int i = 0;
 
