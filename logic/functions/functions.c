7c193b96536c2c16d7e2e83fb3f2bb3e01f1c432
sitoftonic <sitoftonic@gmail.com>
2019-01-05 13:52:23 +0100

    doc: Added comments for every function in functions.c

--- logic/functions/functions.c
+++ logic/functions/functions.c
@@ -3,24 +3,27 @@
 #include <stdlib.h>
 #include <math.h>
 
+// Funció que printa totes les destinacions
 void showAllDestinations(List *l) {
 	int option, i = 0;
+	// Printem el menu fins que la opció sigui correcta
 	do {
 		printSortMenu();
-
+		// Demanem la opció a l'usuari
 		option = askUserForOption();
 	} while (option < 1 || option > MAX_SORTING);
-
+	// Restem una posició a la opció ja que aquesta defineix la posició d'un array
 	option -= 1;
-
+	// Es col·loca el POI a la primera posició de la llista ordenada per option
 	goStart(l, option);
-
+	// Mentre no s'acaba la llista es printa la informació de la destinació
 	while (!isEnd(*l, option)) {
 		printDestination(readPoi(*l, option), ++i, option);
 		goNext(l, option);
 	}
 }
 
+// Funció que compara els preus de les dues destinacions a i b
 int comparePrices(const void * a, const void * b) {
 	double aValue = *((double *) a);
 	double bValue = *((double *) b);
@@ -28,42 +31,44 @@ int comparePrices(const void * a, const void * b) {
 	return (int)round(bValue - aValue);
 }
 
+// Funció que s'encarrega de planificar un viatge
 void planTrip(List *l){
     int i = 0, option, destination;
 	Destination d;
 	char ** map;
-
+	// Es printa la llista de destinacions fins que l'usuari introdueixi una opció vàlida
 	do {
         printDestinationList(l);
+		// Demanem opció a l'usuari
         destination = askUserForOption();
 
         if (destination < 1 || destination > l->n) {
 			printOptionError();
         }
     } while (destination < 1 || destination > l->n);
-
+	// Es printen les opcions mentre l'opció introduida per l'usuari sigui vàlida
     do {
         printPlanMenu();
+		// Demanem opció a l'usuari
         option = askUserForOption();
 
 		if (option < 1 || option > 2) {
 			printOptionError();
 		}
     } while (option < 1 || option > 2);
-
+	// Col·loquem el POI de la llista al principi
 	goStart(l,0);
+	// Avancem per la llista fins arribar a la destinació
 	goNextTimes(l, 0, destination);
+	// Llegim la destinació
 	d = readPoi(*l,0);
 
+	// Escollim funcionalitat en funció de la opció introduida per l'usuari
     switch (option) {
         case 1:
-
             printAverage(getAverageHotelPrice(d));
-
         	qsort(d.hotelPrices, d.nHotels, sizeof(double), comparePrices);
-
             printTop3(d.hotelPrices);
-
             break;
         case 2:
         	map = mapAltitudeTravel(d);
@@ -79,7 +84,7 @@ void planTrip(List *l){
 
 }
 
-
+// Funció que llegeix tota la informació del fitxer f i la introdueix a la llista
 void readWholeFile(FILE * f, List * l) {
 	int nDestinations, i;
 
@@ -91,6 +96,8 @@ void readWholeFile(FILE * f, List * l) {
 	}
 }
 
+// Funció que llegeix tota la informació d'un fitxer nou, borra la informació de la llista
+// i li introdueix la nova informació
 void readNewFile(List * l) {
 	char *input = askUserForPath();
 
@@ -106,6 +113,7 @@ void readNewFile(List * l) {
 
 }
 
+// Funció que executa les funcionalitats del programa
 void execute(int option, List * l) {
 	switch (option) {
 		case 1:
