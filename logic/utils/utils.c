d7f6c2884c7e2f63e1ed4075b5949e4d7f3a9727
sitoftonic <sitoftonic@gmail.com>
2019-01-05 14:00:40 +0100

    doc: Added comments to resize function

--- logic/utils/utils.c
+++ logic/utils/utils.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <memory.h>
 
-
+// Funció que canvia la mida d'una string i la fa més gran
 void resize(char* pointer, int size) {
 	char * new = (char *) malloc(sizeof(char) * size);
 
